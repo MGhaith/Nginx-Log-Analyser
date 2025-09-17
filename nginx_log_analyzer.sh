74629ea04eb637dd40a4f278962b89e3e05516ce
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-17 13:01:37 +0100

    add top 5 adresses

--- nginx_log_analyzer.sh
+++ nginx_log_analyzer.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# Usage: ./nginx_log_analyzer.sh access.log
+
+LOGFILE=$1
+
+# Check if log file is provided and not empty
+if [[ -z "$LOGFILE" || ! -f "$LOGFILE" || ! -s "$LOGFILE" ]]; then
+    echo "Usage: $0 <nginx_access_log_file>"
+    exit 1
+fi
+
+echo "Top 5 IP addresses with the most requests:"
+awk '{print $1}' "$LOGFILE" | sort | uniq -c | sort -nr | head -5 | awk '{print $2 " - " $1 " requests"}'
