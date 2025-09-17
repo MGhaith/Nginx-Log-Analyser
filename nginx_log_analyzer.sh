63010534853caa9c2b1c659d9dccc7409f5f7338
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-17 13:01:56 +0100

    add top requested paths

--- nginx_log_analyzer.sh
+++ nginx_log_analyzer.sh
@@ -12,3 +12,7 @@ fi
 
 echo "Top 5 IP addresses with the most requests:"
 awk '{print $1}' "$LOGFILE" | sort | uniq -c | sort -nr | head -5 | awk '{print $2 " - " $1 " requests"}'
+echo
+
+echo "Top 5 most requested paths:"
+awk '{print $7}' "$LOGFILE" | sort | uniq -c | sort -nr | head -5 | awk '{print $2 " - " $1 " requests"}'
